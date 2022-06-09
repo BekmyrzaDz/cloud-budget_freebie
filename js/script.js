92ac6cf54d563f3d1d32da5f26dc92dd702b9564
Bekmyrza <msn.10110922@gmail.com>
2022-06-09 12:02:36 +0600

    Удалил лишний код

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const button = document.querySelector('.menu__btn'),
   list = document.querySelector('.menu__list');
-console.log(button);
-console.log(list);
 
 button.addEventListener('click', () => {
   list.classList.toggle('menu__list--active');
